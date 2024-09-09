9af1edf2d3adae5264ef6415f9c1638970f4b495
Ashton Hotke <339741886@GoTVDSB.ca>
2024-09-09 09:01:48 -0400

    Add project files.

---
+++ Topic 4 - User Input/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Topic_4___User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string age;
+            Console.WriteLine("Enter your age:");
+            age = Console.ReadLine();
+            Console.WriteLine("Your age is: " + age);
+            Console.ReadLine();
+
+        }
+    }
+}
